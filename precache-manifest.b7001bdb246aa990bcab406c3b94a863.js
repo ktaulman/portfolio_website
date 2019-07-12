b7d5f84db614b8387a32b053c741a82df42f1edc
Kevin Taulman <kevin.taulman@icloud.com>
2019-07-12 13:26:32 -0700

    Updates

---
+++ precache-manifest.b7001bdb246aa990bcab406c3b94a863.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0cdb4906f2aceebe8b16e630feb20a",
+    "url": "/portfolio_website/index.html"
+  },
+  {
+    "revision": "3fddc93fca59ce4024b3",
+    "url": "/portfolio_website/static/css/main.2ad59a81.chunk.css"
+  },
+  {
+    "revision": "bc9e213220f85ecf4864",
+    "url": "/portfolio_website/static/js/2.501d70a8.chunk.js"
+  },
+  {
+    "revision": "3fddc93fca59ce4024b3",
+    "url": "/portfolio_website/static/js/main.75a1d65c.chunk.js"
+  },
+  {
+    "revision": "8db4c5aa3ad52749f359",
+    "url": "/portfolio_website/static/js/runtime~main.9679e305.js"
+  },
+  {
+    "revision": "e097b7b2b16e877b8d8b2dd20d763680",
+    "url": "/portfolio_website/static/media/calendar.e097b7b2.png"
+  },
+  {
+    "revision": "cb4f694ce1d08b9953e9edf1059d1013",
+    "url": "/portfolio_website/static/media/css.cb4f694c.png"
+  },
+  {
+    "revision": "c42043d44e88740dc85b76406fcef61a",
+    "url": "/portfolio_website/static/media/html.c42043d4.png"
+  },
+  {
+    "revision": "afb47678875386aa6e81b65aa0640563",
+    "url": "/portfolio_website/static/media/javascript.afb47678.png"
+  },
+  {
+    "revision": "6d61a6febda762990962761052fe2d64",
+    "url": "/portfolio_website/static/media/jeopardy.6d61a6fe.png"
+  },
+  {
+    "revision": "5e56c1d52c5e17c806122a036a8e0e22",
+    "url": "/portfolio_website/static/media/kevin.5e56c1d5.jpg"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/portfolio_website/static/media/node.f8dab57d.png"
+  },
+  {
+    "revision": "782514cfa1fa62abfdf03a6737283c62",
+    "url": "/portfolio_website/static/media/resume.782514cf.png"
+  },
+  {
+    "revision": "c4489a5b2ae56d1a41428d10e1eb33e1",
+    "url": "/portfolio_website/static/media/twitter.c4489a5b.png"
+  }
+]);
