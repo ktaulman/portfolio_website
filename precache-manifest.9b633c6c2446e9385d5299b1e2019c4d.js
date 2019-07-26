f859aa9aa7d62f52921b495219807348d8960d93
Kevin Taulman <kevin.taulman@icloud.com>
2019-07-25 17:01:08 -0700

    Updates

---
+++ precache-manifest.9b633c6c2446e9385d5299b1e2019c4d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587ffbe11aa84bea5140be8d635d6fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7812dd68e0b35742f08d",
+    "url": "/static/js/2.051a8eab.chunk.js"
+  },
+  {
+    "revision": "5215201d257fca2d7f35",
+    "url": "/static/js/main.8100789a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb4f694ce1d08b9953e9edf1059d1013",
+    "url": "/static/media/css.cb4f694c.png"
+  },
+  {
+    "revision": "c42043d44e88740dc85b76406fcef61a",
+    "url": "/static/media/html.c42043d4.png"
+  },
+  {
+    "revision": "afb47678875386aa6e81b65aa0640563",
+    "url": "/static/media/javascript.afb47678.png"
+  },
+  {
+    "revision": "6d61a6febda762990962761052fe2d64",
+    "url": "/static/media/jeopardy.6d61a6fe.png"
+  },
+  {
+    "revision": "5e56c1d52c5e17c806122a036a8e0e22",
+    "url": "/static/media/kevin.5e56c1d5.jpg"
+  },
+  {
+    "revision": "4768889ec3f933f4159ce7bffdd51f81",
+    "url": "/static/media/logo.4768889e.svg"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/static/media/node.f8dab57d.png"
+  },
+  {
+    "revision": "782514cfa1fa62abfdf03a6737283c62",
+    "url": "/static/media/resume.782514cf.png"
+  },
+  {
+    "revision": "c4489a5b2ae56d1a41428d10e1eb33e1",
+    "url": "/static/media/twitter.c4489a5b.png"
+  }
+]);
