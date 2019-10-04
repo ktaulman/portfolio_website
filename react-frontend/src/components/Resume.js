5e10724d5b9df02510282d57ece1f63e73f7971d
Kevin Taulman <kevin.taulman@icloud.com>
2019-10-04 18:15:04 -0400

    added flavortown back in

--- react-frontend/src/components/Resume.js
+++ react-frontend/src/components/Resume.js
@@ -7,7 +7,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const Resume=()=>{
     return(
     <main className="resume animated fadeIn">
-        <a className="--resume-link --font-black" href="/resume.pdf" download="resume">Download</a>
+        <a className="--resume-link --font-black" href="https://docs.google.com/document/d/1bE7uOB-hGm_9L2BxH0CKEm49CL9aZtQw554JIw8lq-c/edit?usp=sharing" >View,Download</a>
         <Document
             className="--document"
             file="/resume.pdf"
